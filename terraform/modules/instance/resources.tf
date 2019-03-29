7e2dc682f0eccb4f14eee51b87bb64baee071a8b
sharmamukesh49 <mukesh.sharma@closeloop.com>
2019-03-29 13:19:36 +0530

    Route53 all subdomain working with get ip

--- terraform/modules/instance/resources.tf
+++ terraform/modules/instance/resources.tf
@@ -32,7 +32,7 @@ data "aws_route53_zone" "selected" {
   private_zone = false
 }
 
-resource "aws_route53_record" "lhb" {
+resource "aws_route53_record" "lhbsuper" {
   zone_id = "${data.aws_route53_zone.selected.zone_id}"
   name    = "lhbsuperadmin.${data.aws_route53_zone.selected.name}"
   type    = "A"
@@ -43,6 +43,22 @@ resource "aws_route53_record" "lhb" {
 resource "aws_route53_record" "lhbdisp" {
   zone_id = "${data.aws_route53_zone.selected.zone_id}"
   name    = "lhbdispadmin.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["${aws_eip.testInstanceEip.public_ip}"]
+}
+
+resource "aws_route53_record" "lhbapi" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "lhbapis.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["${aws_eip.testInstanceEip.public_ip}"]
+}
+
+resource "aws_route53_record" "lhbdb" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "lhbdb.${data.aws_route53_zone.selected.name}"
   type    = "A"
   ttl     = "60"
   records = ["${aws_eip.testInstanceEip.public_ip}"]
